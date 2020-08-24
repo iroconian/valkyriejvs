aa483ad0f039290d48f73d623a10d89178c3b365
iroconian <66124064+iroconian@users.noreply.github.com>
2020-08-24 15:12:05 -0400

    Screen
    Loading Screen

---
+++ js/config.js
@@ -0,0 +1,182 @@
+//       _           _    _                     _ 
+//      | |         | |  | |                   | |
+//   ___| | ___  ___| | _| |     ___   __ _  __| |
+//  / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+//  \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+//  |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+//     version 2.16
+
+// URL for the server logo
+// Leave it blank to use text instead
+// (Put the files in the images folder)
+// (You can also use external URL)
+var l_serverImage = "logo.png";
+
+// Center logo?
+// 'true' or 'false'
+var l_centerLogo = false;
+
+// Display current map and gamemode?
+// 'true' or 'false'
+var l_displayMapGamemode = true;
+
+// Custom server name configuration
+// Leave it blank for automatic server name
+var l_serverName = "valkyrienetwork";
+
+// Use a video for the background?
+// Otherwise it will use images
+// 'true' or 'false'
+var l_bgVideo = true;
+
+// Default available videos:
+// "aurora.webm"
+// "bluefall.webm"
+// "bluetilefloor.webm"
+// "bluewhitecircles.webm"
+// "bokehlens.webm"
+// "cleanbokeh.webm"
+// "coldnight.webm"
+// "colorfulburst.webm"
+// "dancingstars.webm"
+// "dropsflares.webm"
+// "focusorangebokeh.webm"
+// "glimmering.webm"
+// "glowingrings.webm"
+// "greencircles.webm"
+// "hues.webm"
+// "inthedeep.webm"
+// "modernflourish.webm"
+// "orangeaura.webm"
+// "warminside.webm"
+// "yellowgreen.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format 
+// (Place the videos in the backgrounds/videos folder)
+// (You can also use external URL)
+var l_background = "hues.webm";
+
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// 'true' or 'false'
+var l_bgImageMapBased = false;
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the files in the backgrounds/images folder)
+// (You can also use external URL)
+var l_bgImages = [
+	"vn_1.jpg",
+	"vn_2.jpg",
+	"vn_3.jpg",
+	"vn_4.jpg",
+	"vn_5.jpg",
+	"vn_6.jpg",
+	"vn_7.jpg",
+	"vn_8.jpg",
+	"vn_9.jpg",
+	"vn_10.jpg",
+	"vn_11.jpg",
+	"vn_12.jpg",
+	"vn_13.jpg",
+	"vn_14.jpg",
+	"vn_15.jpg",
+	"vn_16.jpg",
+	"vn_17.jpg",
+	"vn_18.jpg"
+];
+
+// (Image-background only)
+// Random background images order?
+// 'true' or 'false'
+var l_bgImagesRandom = true;
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+var l_bgImageDuration = 2500;
+
+// (Image-background only)
+// Image background fade velocity
+// in milliseconds
+var l_bgImageFadeVelocity = 1000;
+
+// Enable background overlay?
+// 'true' or 'false'
+var l_bgOverlay = false;
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+var l_bgDarkening = 80;
+
+// Play music during the loading screen?
+// 'true' or 'false'
+var l_music = true;
+
+// Display current music name?
+// 'true' or 'false'
+var l_musicDisplay = true;
+
+// Music playlist
+// Add as many youtube ids/urls or ogg files you want
+// (Place the ogg files in the music folder)
+// (You can also use external URL for the ogg files)
+var l_musicPlaylist = [
+	{ogg: "sith_code.ogg", name: "Learning the Sith Code"},
+	{ogg: "the_sacrifice.ogg", name: "Kavinsky - Night Call"},
+];
+
+// Random music order?
+// 'true' or 'false'
+var l_musicRandom = true;
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+var l_musicVolume = 5;
+
+// Enable custom messages?
+// 'true' or 'false'
+var l_messagesEnabled = true;
+
+// Enter your custom messages below
+var l_messages = [
+	"Welcome to VALKYRIE Networks Jedi vs Sith",
+    "Thank You for joining us Today!",
+    "Join us on Discord for Event information",
+    "Zeus Iroc and VALKYRIE welcome you to our server",
+    "Life is too important to be taken seriously. -Oscar Wilde",
+    "The Galactic War is upon us",
+    "We are making constant improvements to our server!",
+];
+
+// Random message order?
+// 'true' or 'false'
+var l_messagesRandom = true;
+
+// Delay between message changes
+// in milliseconds
+var l_messagesDelay = 5000;
+
+// Messages fade time
+// in milliseconds
+var l_messagesFade = 1000;
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// 'true' or 'false'
+var l_showErrorsIngame = true;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};
